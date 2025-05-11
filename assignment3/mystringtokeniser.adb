18b94941ef5ca3ba2555a75df3dd9e413a02f289
Josh Feng <cnjosh90098@gmail.com>
2025-05-11 16:58:29 +1000

    Finished Task 1.2

--- assignment3/mystringtokeniser.adb
+++ assignment3/mystringtokeniser.adb
@@ -15,12 +15,22 @@ package body MyStringTokeniser with SPARK_Mode is
       end if;
       Index := S'First;
       while OutIndex <= Tokens'Last and Index <= S'Last and Processed < Tokens'Length loop
+         -- It is check for all the elements that have been store into the Tokens, i.e., Tokens'First..OutIndex-1
+         -- 1. For each of start index of token must greater or equal to the first index of S, avoid access outside of boundary
+         -- 2. Each Token length must greater than 0, otherwise will access illegal index range
+         -- 3. If 1 and 2 both true, then check,
+         --    the posistion of the last character of the token must smaller or equal to the last index of the string S
+         --    ensure that token not access the right boundary of the string to access illegal memory
          pragma Loop_Invariant
            (for all J in Tokens'First..OutIndex-1 =>
               (Tokens(J).Start >= S'First and
                    Tokens(J).Length > 0) and then
             Tokens(J).Length-1 <= S'Last - Tokens(J).Start);
 
+         -- Each time, OutIndex equal to the index of first token + number f processed
+         -- Ensuring that OutIndex not duplicates, no able to write to the same position twice
+         -- ensuring that OutIndex not missing any position in the array, and
+         -- ensuring that OutIndex not out of boundary and always within legal index range
          pragma Loop_Invariant (OutIndex = Tokens'First + Processed);
 
          -- look for start of next token
