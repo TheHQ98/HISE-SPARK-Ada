b6249cbda6f437e78397a878861f62f82df76fd4
Josh Feng <cnjosh90098@gmail.com>
2025-05-14 13:33:10 +1000

    added post condition for Is_Legal_Number fn in utils

--- assignment3/utils.ads
+++ assignment3/utils.ads
@@ -6,5 +6,8 @@ package Utils with SPARK_Mode is
           (for all I in Token'Range => Token (I) in '0' .. '9')));
 
    -- for checking memory address
-   function Is_Legal_Number(I : Integer) return Boolean;
+   function Is_Legal_Number(I : Integer) return Boolean with
+    Post =>
+      (Is_Legal_Number'Result = True  and then I in 1 .. 256)    or
+      (Is_Legal_Number'Result = False and then I not in 1 .. 256);
 end Utils;
